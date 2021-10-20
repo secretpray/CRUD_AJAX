ea3760b3193200fd3069c58df8c133725e84ccc5
secretpray <Renovatio100>
2021-10-20 22:02:09 +0300

    Add all CRUD AJAX to Users resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence:true
   validates :email, :address, presence:true, uniqueness: {case_sensitive: false}
+
+  scope :recent, -> { User.order(created_at: :desc) }
 end
