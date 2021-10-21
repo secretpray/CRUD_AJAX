65a16145db421e04ecc671a4e32a4f32125114b5
secretpray <Renovatio100>
2021-10-21 21:49:12 +0300

    Small ref AJAX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence:true
-  validates :email, :address, presence:true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true
+  validates :email, :address, presence: true, uniqueness: {case_sensitive: false}
 
   scope :recent, -> { User.order(created_at: :desc) }
 end
