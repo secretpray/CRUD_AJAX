ac93d5118ad3e3842a5f63cfbb45cfd93505ebc0
secretpray <Renovatio100>
2021-10-29 15:34:49 +0300

    Add footer, apply light green theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
+import "@fortawesome/fontawesome-free/css/all"
 
 // popover script
 require('utilities/popover')
